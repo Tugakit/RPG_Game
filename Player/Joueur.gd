747219fd136c0ca2392beca4e9ef5d38d6044007
Tugakit <tuga.kit@gmail.com>
2021-05-11 15:28:36 +0200

    Added movements and a simple background

--- Player/Joueur.gd
+++ Player/Joueur.gd
@@ -4,10 +4,11 @@ const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
 
-var velocity = Vector2.ZERO	
+var velocity = Vector2.ZERO
 
-func _ready():
-	print("Hellow world +")
+onready var  animationPlayer = $AnimationPlayer
+onready var  animationTree = $AnimationTree
+onready var  animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -16,9 +17,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-		print(velocity)
 	else:
+		animationState.travel("idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 
